25d0f1ca8a61dad407b6c6ca9c7dd01e0d6912c6
Santosh Pujeri <your-email@example.com>
2026-07-24 23:01:09 +0530

    Initial Spring Boot project setup and project structure

---
+++ src/test/java/com/santosh/aiworkflowplatform/AiWorkflowPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosh.aiworkflowplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWorkflowPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
