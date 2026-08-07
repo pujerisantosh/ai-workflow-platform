6096b95330ec79088effc17b4cb4e649bcda2416
Santosh Pujeri <16santoshpujari@gmail.com>
2026-08-08 01:17:58 +0530

    feat(AWP-101): add user registration foundation

--- src/main/java/com/santosh/aiworkflowplatform/service/AuthService.java
+++ src/main/java/com/santosh/aiworkflowplatform/service/AuthService.java
@@ -3,9 +3,8 @@ package com.santosh.aiworkflowplatform.service;
 import com.santosh.aiworkflowplatform.dto.request.RegisterRequest;
 import com.santosh.aiworkflowplatform.dto.response.AuthResponse;
 
-public interface AuthService {
+public   interface AuthService {
 
-    String register();
 
     AuthResponse register(RegisterRequest request);
 }
--- src/main/java/com/santosh/aiworkflowplatform/service/impl/AuthServiceImpl.java
+++ src/main/java/com/santosh/aiworkflowplatform/service/impl/AuthServiceImpl.java
@@ -2,19 +2,45 @@ package com.santosh.aiworkflowplatform.service.impl;
 
 import com.santosh.aiworkflowplatform.dto.request.RegisterRequest;
 import com.santosh.aiworkflowplatform.dto.response.AuthResponse;
+import com.santosh.aiworkflowplatform.entity.User;
+import com.santosh.aiworkflowplatform.repository.UserRepository;
 import com.santosh.aiworkflowplatform.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    @Override
-    public String register() {
-        return "User registered successfully";
-    }
+    private final UserRepository userRepository;
 
     @Override
     public AuthResponse register(RegisterRequest request) {
-        return null;
+
+        Optional<User> existingUser =
+                userRepository.findByEmail(request.getEmail());
+
+        if (existingUser.isPresent()) {
+            throw new RuntimeException("User already exists");
+        }
+
+        User user = new User();
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+
+        // Set other fields if they exist in your User entity
+        // user.setRole(Role.USER);
+        // user.setCreatedAt(LocalDateTime.now());
+        // user.setUpdatedAt(LocalDateTime.now());
+
+        userRepository.save(user);
+
+        return new AuthResponse("User registered successfully");
     }
 }
+
